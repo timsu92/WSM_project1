687a2f21d19cdee3f67ee6f92d03beca36f0e330
timsu92 <33785401+timsu92@users.noreply.github.com>
2024-10-23 18:33:00 +0800

    feat(main): basic completion of problem 4

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from itertools import product
-from multiprocessing.spawn import prepare
+import json
 import os
+from typing import Dict, List
+
+from tqdm import tqdm
+
 from lib.vectorSpace import VectorSpace
 
 
@@ -35,7 +39,10 @@ for similarity, weighting in product(["cos", "euclidean"], ["tf", "tf-idf"]):
     ranking = vecSpace.queryByVector(queryVec, weighting, similarity, 1)[0]
     with open(os.path.join(os.path.dirname(__file__), "EnglishNews", ranking[0])) as f:
         feedback = vecSpace.preProc.tokenize((f.read(),), "en")
-    feedback_pos = filter(lambda x: x[1].startswith(("N", "V")), next(vecSpace.preProc.pos(feedback, "en")))
+    feedback_pos = filter(
+        lambda x: x[1].startswith(("N", "V")),
+        next(vecSpace.preProc.pos(feedback, "en")),
+    )
     feedback = " ".join(map(lambda x: x[0], feedback_pos))
     queryVec = queryVec + vecSpace.buildTfVecFromText(feedback, "en") * 0.5
 
@@ -56,8 +63,7 @@ vecSpace.addDocsInFolder(
 print("Corpus created!\n")
 
 query = (
-    input("Type query for problem 3 here (blank for '資安 遊戲')\n> ")
-    or "資安 遊戲"
+    input("Type query for problem 3 here (blank for '資安 遊戲')\n> ") or "資安 遊戲"
 )
 
 for similarity, weighting in product(["cos"], ["tf", "tf-idf"]):
@@ -67,3 +73,60 @@ for similarity, weighting in product(["cos"], ["tf", "tf-idf"]):
     for ranking in rankings:
         print(os.path.basename(ranking[0]), ranking[1], sep="\t")
     print("--------------------------------------")
+
+print("####### problem 4 #######")
+
+print("creating new corpus and adding smaller_dataset to corpus...")
+vecSpace = VectorSpace()
+vecSpace.addDocsInFolder(
+    os.path.join(os.path.dirname(__file__), "smaller_dataset", "collections"), lang="en"
+)
+print("Corpus created!")
+print("Reading answers from smaller_dataset/rel.tsv...")
+with open(os.path.join(os.path.dirname(__file__), "smaller_dataset", "rel.tsv")) as f:
+    answers: Dict[str, List[int]] = {
+        line[0]: json.loads(line[1]) for line in map(lambda x: x.strip().split("\t"), f)
+    }
+
+nQueries = len(
+    os.listdir(os.path.join(os.path.dirname(__file__), "smaller_dataset", "queries"))
+)
+MRR = 0
+MAP = 0
+recall = 0
+print("Calculating query scores...")
+for queryFName in tqdm(
+    os.listdir(os.path.join(os.path.dirname(__file__), "smaller_dataset", "queries"))
+):
+    if queryFName.endswith(".txt"):
+        with open(
+            os.path.join(
+                os.path.dirname(__file__), "smaller_dataset", "queries", queryFName
+            )
+        ) as f:
+            query = f.read()
+        rankings = vecSpace.queryByText(query, "tf-idf", "cos", "en", 10)
+        corrects = 0
+        precision = 0
+        mrrFound = False
+        for i, ranking in enumerate(rankings):
+            if (
+                int(os.path.basename(ranking[0]).split(".")[0][1:])
+                in answers[queryFName.split(".")[0]]
+            ):
+                # the retrieved document is relevant
+                if not mrrFound:
+                    MRR += 1.0 / (i + 1)
+                    mrrFound = True
+                corrects += 1
+                precision += corrects / (i + 1)
+        if corrects > 0:
+            MAP += precision / corrects
+        recall += corrects / len(answers[queryFName.split(".")[0]])
+MRR /= nQueries
+MAP /= nQueries
+recall /= nQueries
+
+print("MRR@10", MRR, sep="\t")
+print("MAP@10", MAP, sep="\t")
+print("RECALL@10", recall, sep="\t")
